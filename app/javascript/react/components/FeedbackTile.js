54b3aac49f4c1304397d43562742fb6e5d640582
JulianBaxandall <jlkbax@gmail.com>
2022-08-23 20:57:15 -0400

    Bundler fix

--- app/javascript/react/components/FeedbackTile.js
+++ app/javascript/react/components/FeedbackTile.js
@@ -1,11 +1,31 @@
 import React, {useState, useEffect} from "react";
 
 const FeedbackTile = (props) => {
+    const resolvedString = props.resolved.toString()
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        let newResolved = !props.resolved
+        const formPayload = {
+                resolved: newResolved,
+                id : props.id
+                }
+        props.toggleResolved(event, formPayload)
+    }
+
     return (
         <div className = "card cell small-4">
             <h5>{props.title}</h5>
             <p>Category: {props.category}</p>
             <p>Body: {props.body}</p>
+            <p>Resolved?: {resolvedString}</p>
+            <form onSubmit = {handleSubmit}>
+                <input 
+                    type="submit" 
+                    value="Toggle Resolved"
+                    className = "button"
+                    />            
+            </form>
         </div>
     )
 }
