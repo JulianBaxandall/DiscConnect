87caf8bcb4cbca6d67a17a568807d9ce2532fcf5
JulianBaxandall <jlkbax@gmail.com>
2022-09-21 15:27:27 -0400

    created leaderboard differentiation for workout types

--- app/javascript/react/components/TeamWorkouts.js
+++ app/javascript/react/components/TeamWorkouts.js
@@ -36,11 +36,33 @@ const TeamWorkouts = (props) => {
     })
 
     return (
-        <div className = "independence">
-            <h3>Teams Workouts: {team.name}</h3>
-            <div className = "container">
-                {workouts}
+        <div className="grid-x grid-padding-x">
+            
+            <div className = "cell small-8 independence">
+                <h3>Teams Workouts: {team.name}</h3>
+                <div className = "container">
+                    <div className = "row independence">
+                        <div className = "name">
+                            <h5 className = "name independence">Name:</h5>
+                        </div>
+                        <div className = "score">
+                            <p className = "score independence">T</p>
+                            <p className = "score independence">L</p>
+                            <p className = "score independence">R</p>
+                            <p className = "score independence">O</p>
+                        </div>
+                    </div>
+                    {workouts}
+                </div>
             </div>
+            <div className="cell small-2 key">
+                <h5>Key:</h5>
+                <p>T = Throwing</p>
+                <p>L = Lifting</p>
+                <p>R = Running</p>
+                <p>O = Other</p>
+            </div>
+
         </div>
     )
 }
--- app/javascript/react/components/UserWorkoutsTile.js
+++ app/javascript/react/components/UserWorkoutsTile.js
@@ -1,13 +1,38 @@
 import React, {useState, useEffect} from "react";
 
 const UserWorkoutTile = (props) => {
+    const workouts = props.user.workouts
+    
+    let throwing = 0
+    let lifting = 0
+    let running = 0
+    let film = 0
+    let other = 0
+
+    for (let i = 0; i < workouts.length; i++) {
+        if (workouts[i]['workout_type'] === 'throwing'){
+            throwing ++
+        } else if (workouts[i]['workout_type'] === 'lifting'){
+            lifting ++
+        } else if (workouts[i]['workout_type'] === 'running'){
+            running ++
+        } else if (workouts[i]['workout_type'] === 'film'){
+            film ++
+        } else if (workouts[i]['workout_type'] === 'other'){
+            other ++
+        }
+    }
+
     return (
         <div className = "row independence">
             <div className = "name">
                 <h5 className = "name independence">{props.user.name}</h5>
             </div>
             <div className = "score">
-                <p className = "score independence">{props.user.workouts.length}</p>
+                <p className = "score independence">{throwing}</p>
+                <p className = "score independence">{lifting}</p>
+                <p className = "score independence">{running}</p>
+                <p className = "score independence">{other}</p>
             </div>
         </div>
     )
